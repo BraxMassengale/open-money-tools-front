4f4e463a4270987da2cab37e1672c149058afa7b
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-06-07 23:50:39 +0200

    update

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+
+const whitelist = ['http://localhost:3000']; // list of allow domain
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.indexOf(origin) === -1) {
+      let msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}
+
+
+app.use(cors(corsOptions));
 
 app.use(express.static('./dist/tfmApp'));
 
