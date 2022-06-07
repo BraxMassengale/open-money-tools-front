279237973966ce5c34853776c8f5a64c3d281e66
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-06-08 00:03:05 +0200

    update

--- server.js
+++ server.js
@@ -4,26 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-
-const whitelist = ['https://open-money-tools-front.herokuapp.com, https://open-money-tools.herokuapp.com', "http://openmoney.tools"]; // list of allow domain
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (whitelist.indexOf(origin) === -1) {
-      let msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}
-
-
-app.use(cors(corsOptions));
+app.options('*', cors())
 
 app.use(express.static('./dist/tfmApp'));
 
